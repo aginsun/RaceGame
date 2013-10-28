e0f2a596ff3b73b02cc5349d1f491d7bee2148cd
3kip3 <j.p_ludema@hotmail.com>
2013-10-28 20:43:49 +0100

    car

--- src/RaceGame/RaceGame/Car.cs
+++ src/RaceGame/RaceGame/Car.cs
@@ -15,7 +15,7 @@ namespace RaceGame
     	public float Acceleration;
     	public double Speed;
        	public float Health;
-		public float Direction;
+		public double Direction;
 
         public Car()
         {
@@ -28,34 +28,12 @@ namespace RaceGame
 		    Direction = 90;
         }
 
+        
         public void Update(GameTime gameTime)
-        {
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
-                this.PosY -= (int)(Speed + 1);
-                if (Speed < 10)
-                    Speed += 0.05;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.S))
-            {
-                this.PosY += 1;
-
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                this.PosX -= 1;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
-                this.PosX += 1;
-            }
-        }
-
-        /*public void Update(GameTime gameTime)
         {
 	        Fuel = Fuel - (0.05 * Speed); 
 	        KeyboardState kbstate = Keyboard.GetState();
-            if (kbstate.IsKeyDown(Keys.Up)) 
+            if (kbstate.IsKeyDown(Keys.W)) 
 	        {
                 if(collision == collision.road)
 	            {
@@ -96,7 +74,7 @@ namespace RaceGame
 			    Speed+= 20;
 			    Health+= -10 ;
 			}
-            else if (kbstate.IsKeyDown(Keys.Down))
+            else if (kbstate.IsKeyDown(Keys.S))
 	        {
                 if(collision == collision.road)
 	            {
@@ -144,8 +122,106 @@ namespace RaceGame
 	           Speed+= -20;
 		       Health+= -10;
 	        }
+         
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                this.PosY -= (int)(Speed + 1);
+                if (Speed < 10)
+                    Speed += 0.05;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                this.PosY += 1;
 
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A) && Speed !=0 )
+            {
+                if (Direction >= 0 && Direction <= 44)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                }
+                else if (Direction >= 45 && Direction <= 89)
+                {
+                    this.PosX -= 1;
+                    this.PosY += 2;
+                }
+                else if (Direction >= 90 && Direction <= 134)
+                {
+                    this.PosX -= 1;
+                    this.PosY += 0;
+                } 
+                else if (Direction >= 135 && Direction <= 179)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                } 
+                else if (Direction >= 180 && Direction <= 224)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                }
+                else if (Direction >= 225 && Direction <= 269)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                }
+                else if (Direction >= 270 && Direction <= 314)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                }
+                else if (Direction >= 315 && Direction <= 359)
+                {
+                    this.PosX += 0;
+                    this.PosY += 0;
+                }
+            }
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.D) && Speed != 0)
+            {
+                    if (Direction >= 0 && Direction <= 44)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 45 && Direction <= 89)
+                    {
+                        this.PosX -= 1;
+                        this.PosY += 2;
+                    }
+                    else if (Direction >= 90 && Direction <= 134)
+                    {
+                        this.PosX -= 1;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 135 && Direction <= 179)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 180 && Direction <= 224)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 225 && Direction <= 269)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 270 && Direction <= 314)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+                    else if (Direction >= 315 && Direction <= 359)
+                    {
+                        this.PosX += 0;
+                        this.PosY += 0;
+                    }
+             }
 
-        }*/
+        }
     }
 }
