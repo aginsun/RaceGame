a6482edc148761329ec530d7566ba060b350476a
aginsun <aginsun@gmail.com>
2013-10-30 12:46:33 +0100

    Powerup

---
+++ src/RaceGame/RaceGame/Powerup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaceGame
+{
+    class Powerup
+    {
+        int posx;
+        int posy;
+        public int number;
+
+        public Powerup(int number)
+        {
+            posx = 0;
+            posy = 0;
+            this.number = number;
+        }
+
+        public void onCollision(Car car)
+        {
+            car.Fuel += 200;
+            TrackHandler.getInstance().listPowerups.RemoveAt(number);
+        }
+
+        public void setPosition(int x, int y)
+        {
+            posx = x;
+            posy = y;
+        }
+    }
+}
